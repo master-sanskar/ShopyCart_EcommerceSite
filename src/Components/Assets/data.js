22c82faac5c6ba704ba4224dc239f251a2e0ccab
unknown <sanskarchaurey2000@gmail.com>
2023-11-29 10:39:58 +0530

    first commit

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id: 1,
+    name: "Full Sleeve Jackets",
+    image: p1_img,
+    new_price: 499.00,
+    old_price: 999.00,
+  },
+  {
+    id: 2,
+    name: "Half Sleeve Shirt",
+    image: p2_img,
+    new_price: 299.00,
+    old_price: 499.00,
+  },
+  {
+    id: 3,
+    name: "Striped Sleeve Girl Dress",
+    image: p3_img,
+    new_price: 99.00,
+    old_price: 199.00,
+  },
+  {
+    id: 4,
+    name: "Striped Sleeve Overlap Blouse",
+    image: p4_img,
+    new_price: 199.00,
+    old_price: 299.00,
+  },
+];
+
+export default data_product;
